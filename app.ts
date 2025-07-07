e8028787cd370a1d4cc7cf6f62046e634841fab0
Kavindu Senesh <ksenesh5@gmail.com>
2025-07-07 22:17:45 +0530

    initialize express application with middleware setup

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+}));
+
+// app.use();
+
+export default app;
+
