be312491fba7072ea7e35cb1ff72dac4b1ebe6aa
Kavindu Senesh <ksenesh5@gmail.com>
2025-07-11 14:49:14 +0530

    add auth routes and schema types

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import authRoutes from "./src/routes/authRoutes";
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ app.use(cors({
     credentials: true,
 }));
 
-// app.use();
+app.use('api/v1/auth', authRoutes);
 
 export default app;
 
---
+++ src/types/SchemaTypes.ts
@@ -0,0 +1,39 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+export interface ICustomer extends Document{
+    name: string;
+    email: string;
+    passwordHash: string;
+    phone: string;
+    address: string;
+    cartList: mongoose.Types.ObjectId[];
+    loyaltyPoints: number;
+    isBanned: boolean;
+    createdAt: Date;
+    UpdatedAt: Date;
+}
+
+export interface IBranch extends Document{
+    name: string;
+    address: string;
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IEmployee extends Document{
+    name: string;
+    email: string;
+    passwordHash: string;
+    phone: string;
+    jobTitle: string;
+    role: string;
+    branchId: IBranch['_id'];
+    active: boolean;
+    joinedDate: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
